047f885a804d836bec58dd16802d9803da0f5760
000EZE000 <ezequiel.ignacio.sosa@gmail.com>
2022-12-15 20:58:25 -0300

    css study terminado

---
+++ components/modal/modal.tsx
@@ -0,0 +1,24 @@
+
+import Image from "next/image"
+import style from "./style/style.module.css"
+
+interface prop {
+    openModal: Function
+    image: string
+    switchModal: boolean
+}
+
+export default function Modal({ openModal, image, switchModal }: prop): JSX.Element {
+    return <div>{switchModal && <div className={style.container_modal}><div className={style.modal}>
+        <div className={style.container_button_modal}><button className={style.button_modal} onClick={() => openModal()}>✚</button></div>
+        <Image
+            className={style.image_modal}
+            src={image}
+            width='900'
+            height='400'
+            alt='certificate'
+        />
+    </div>
+    </div>}
+    </div>
+}
--- components/studies/studiesComp.tsx
+++ components/studies/studiesComp.tsx
@@ -2,79 +2,90 @@ import HeadComp from '@components/head/headComp'
 import Image from 'next/image'
 import Nav from '@components/navbar/navbarComp'
 import Footer from '@components/footer/footerComp'
-import Link from 'next/link'
+import { useState } from 'react'
+import Modal from '@components/modal/modal'
+import style from './style/style.module.css'
 export default function StudiesComp(): JSX.Element {
-    return <div>
+    interface Iswitch { certificate: boolean, mentions: boolean }
+    const mySwitch: Iswitch = { certificate: false, mentions: false }
+    const [switchModal, setSwitchModal] = useState(mySwitch)
+    const openModalCertificate = () => setSwitchModal({ ...switchModal, certificate: !switchModal.certificate })
+    const openModalMentions = () => setSwitchModal({ ...switchModal, mentions: !switchModal.mentions })
+    return <div className={style.div_father}>
         <HeadComp
             title='Estudios'
             ico="eze.ico"
         />
         <Nav />
         <main>
-            <h1>Estudios</h1>
             <section>
                 <header>
-                    <h2>Bootcamp</h2>
-                    <Image
-                        src='/../public/static/logo-studies/HENRY.png'
-                        width='1000'
-                        height='200'
-                        alt='Henry'
-                    />
+                    <h2 className={style.title}>Bootcamp</h2>
+                    <div className={style.container_image}>
+                        <Image
+                            src='https://res.cloudinary.com/dqhbskhe7/image/upload/v1671060647/logo-studies/rs7lpjzvaohjxu0xq3ja.png'
+                            width='800'
+                            height='200'
+                            alt='Henry'
+                            className={style.image_henry}
+                        />
+                    </div>
                 </header>
-                <article>
-                    <h2>Titulo obtenido</h2>
-                    <Image
-                        src='/../public/static/certificate/CertificadoFullStack.png'
-                        width='500'
-                        height='500'
-                        alt='certificate'
-                    />
-                    <article>
-                        <ul>
-                            <li>CSS</li>
-                            <li>HTML</li>
-                            <li>Javascript</li>
-                            <li>React</li>
-                            <li>Redux</li>
-                            <li>Express</li>
-                            <li>Sequilize</li>
-                            <li>Postgres</li>
-                        </ul>
-                    </article>
+                <h2 className={style.titule_tecnologie}>Tecnologias Aprendidas</h2>
+                <article className={style.container_span}>
+                    {["CSS", "HTML", "Javascript", "React", "Redux", "Git",
+                        "Express", "Sequilize", "Postgres", "NodeJs"
+                    ].map(tecno => <span className={style.span}>{tecno}</span>)
+                    }
                 </article>
-                <article>
-                    <p>Duracion: 4 meses</p>
-                    <Link href='https://certificates.soyhenry.com/cert?id=179ebaf8-eec5-4196-be85-854fd353773a' >Certificado</Link>
+                <hr />
+                <h2 className={style.titule_tecnologie}>Habilidades Blandas</h2>
+                <article className={style.container_span}>
+                    {["Pair Programing", "Code Review", "Presentacion de Proyecto (Helthy)",
+                        "Trabajo grupal (Sirun)"
+                    ].map(tecno => <span className={style.span}>{tecno}</span>)
+                    }
+                </article>
+                <article className={style.container_button}>
+                    <button onClick={openModalCertificate} className={style.button}>Certificado de Graduacion</button>
+                    <button onClick={openModalMentions} className={style.button}>Menciones Honorificas</button>
+
                 </article>
             </section>
             <hr />
             <section>
                 <header>
-                    <h3>Autodidacta</h3>
-                    <Image
-                        src='/../public/static/logo-studies/autodidacta.jpg'
-                        width='1000'
-                        height='400'
-                        alt='autodidacta'
-                    />
+                    <h2 className={style.title_autodidact}>Autodidacta</h2>
+                    <div className={style.container_image}>
+                        <Image
+                            src='https://res.cloudinary.com/dqhbskhe7/image/upload/v1670707786/logo-studies/rndnz2gfl86ctasxxowf.jpg'
+                            width='1000'
+                            height='400'
+                            alt='autodidacta'
+                            className={style.image_autodidact}
+                        />
+                    </div>
                 </header>
 
 
-                <article>
-                    <ul>
-                        <li>Next js</li>
-                        <li>Prisma (ORM)</li>
-                        <li>Redux Toolkit-Persit</li>
-                        <li>Typescript</li>
-                        <li>NextAuth</li>
-                        <li>Auth0</li>
-                        <li>Cloudinary</li>
-                        <li>Nodemailer</li>
-                    </ul>
+                <article className={style.container_span}>
+                    {["Next js", "Prisma (ORM)", "Redux Toolkit-Persit",
+                        "Typescript", "NextAuth", "Auth0", "Cloudinary", "Nodemailer", "Api Mercado Pago"
+                    ].map(tecno => <span className={style.span_autodidact}>{tecno}</span>)
+                    }
                 </article>
             </section>
         </main>
+        <Modal
+            openModal={openModalCertificate}
+            switchModal={switchModal.certificate}
+            image={'https://res.cloudinary.com/dqhbskhe7/image/upload/v1670707785/certificate/cjigyelajoflmecwmtdk.png'}
+        />
+        <Modal
+            openModal={openModalMentions}
+            switchModal={switchModal.mentions}
+            image={'https://res.cloudinary.com/dqhbskhe7/image/upload/v1671145858/certificate/azrvqdnx6amgmbsjg9lq.png'}
+        />
         <Footer />
     </div>
 
