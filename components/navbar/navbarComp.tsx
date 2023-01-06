d23ddb1cc8ae18f1304e72549ed8f11e9ef9a1f7
000EZE000 <ezequiel.ignacio.sosa@gmail.com>
2023-01-06 18:12:22 -0300

    probando response

--- components/navbar/navbarComp.tsx
+++ components/navbar/navbarComp.tsx
@@ -1,6 +1,8 @@
 import style from './style/style.module.css'
 import Link from 'next/link'
+import { useState } from 'react'
 export default function Nav(): JSX.Element {
+    const [switchNav, setSwitchNav] = useState(false)
     return <nav className={style.nav}>
         <div className={style.container}>
             <Link className={style.link} href={'/'}>Inicio</Link>
@@ -9,5 +11,24 @@ export default function Nav(): JSX.Element {
             <Link className={style.link} href={'/studies'}>Estudios</Link>
             <Link className={style.link} href={'/contact'}>Enviame un email</Link>
         </div>
+        <div className={style.nav_response_close}>
+            <button
+                className={style.button_response}
+                onClick={() => {
+                    setSwitchNav(!switchNav)
+
+                }}>X</button>
+        </div>
+        {switchNav && <div>
+            <div className={style.nav_response_open}>
+                <Link className={style.link} href={'/'}>Inicio</Link>
+                <Link className={style.link} href={'/about'}>Sobre Mi</Link>
+                <Link className={style.link} href={'/skills'}>Habilidades</Link>
+                <Link className={style.link} href={'/studies'}>Estudios</Link>
+                <Link className={style.link} href={'/contact'}>Enviame un email</Link>
+            </div>
+
+        </div>
+        }
     </nav>
 }
