0c1cea38f6a331e67828fe46145716e56f3398e9
000EZE000 <ezequiel.ignacio.sosa@gmail.com>
2023-02-07 17:36:26 -0300

    Refactor agree interface replace functions by methods

--- components/contact/controller/validateEmail.ts
+++ components/contact/controller/validateEmail.ts
@@ -1,10 +1,15 @@
-export const email = (string: string = "") =>
-  /([a-z0-9]{2,15})[@]([a-z0-9]{1,5})+[.][a-z]*/gi.test(string);
-
-export const body = (string: string = "") => {
-  return /(^[a-z0-9!-@]{1,23})+\s([a-z0-9]+).+/gi.test(string);
+const regExpEmail = /([a-z0-9]{2,15})[@]([a-z0-9]{1,5})+[.][a-z]*/gi;
+const regExpName = /(^[a-z0-9!-@]{1,23})+\s([a-z0-9]+).+/gi;
+const regExpBody = /(^[a-z0-9!-@]{1,15})+\s([a-z0-9]+).+/gi;
+interface InterfaceValidator {
+  isEmail: (string: string) => boolean;
+  isBody: (string: string) => boolean;
+  isName: (string: string) => boolean;
+}
+const ValidatorEmail: InterfaceValidator = {
+  isEmail: (email) => regExpEmail.test(email),
+  isBody: (body) => regExpBody.test(body),
+  isName: (name) => regExpName.test(name),
 };
 
-export const name = (string: string = "") => {
-  return /(^[a-z0-9!-@]{1,15})+\s([a-z0-9]+).+/gi.test(string);
-};
+export default ValidatorEmail;
