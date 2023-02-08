a98189fa315594d147a90043895bee68e6d32eff
000EZE000 <ezequiel.ignacio.sosa@gmail.com>
2023-02-08 14:45:26 -0300

    change refactor

--- components/contact/controller/validateEmail.ts
+++ components/contact/controller/validateEmail.ts
@@ -1,15 +1,19 @@
-const regExpEmail = /([a-z0-9]{2,15})[@]([a-z0-9]{1,5})+[.][a-z]*/gi;
-const regExpName = /(^[a-z0-9!-@]{1,23})+\s([a-z0-9]+).+/gi;
-const regExpBody = /(^[a-z0-9!-@]{1,15})+\s([a-z0-9]+).+/gi;
 interface InterfaceValidator {
   isEmail: (string: string) => boolean;
   isBody: (string: string) => boolean;
   isName: (string: string) => boolean;
 }
-const ValidatorEmail: InterfaceValidator = {
-  isEmail: (email) => regExpEmail.test(email),
-  isBody: (body) => regExpBody.test(body),
-  isName: (name) => regExpName.test(name),
+const ValidatorForm: InterfaceValidator = {
+  isEmail: (email) =>
+    !(!email.includes("@")
+      ? false
+      : email.length < 4
+      ? false
+      : email.split("@")[1].length < 2
+      ? false
+      : true),
+  isBody: (body) => body.length < 12,
+  isName: (name) => name.length < 2,
 };
 
-export default ValidatorEmail;
+export default ValidatorForm;
