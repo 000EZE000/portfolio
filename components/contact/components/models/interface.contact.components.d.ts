f5c90fb4f5bb1fa5ebbf95a4c486ff120853bcd2
000EZE000 <ezequiel.ignacio.sosa@gmail.com>
2023-02-08 14:42:52 -0300

    agree type file interface.contact.components

--- components/contact/components/models/interface.contact.components.d.ts
+++ components/contact/components/models/interface.contact.components.d.ts
@@ -6,4 +6,13 @@ export interface InterfacePropEmail {
   form: InterfaceForm;
 }
 
-export type typePropError = Omit<InterfacePropEmail, "setForm">;
+export interface InterfacePropError
+  extends Omit<InterfacePropEmail, "setForm"> {
+  message: string;
+  validation: "isEmail" | "isBody" | "isName";
+  input: "email" | "name" | "body";
+}
+
+export interface InterfacePropSend extends InterfacePropEmail {}
+
+export type TypeStateImage = "success" | "process" | "transparent" | "fail";
