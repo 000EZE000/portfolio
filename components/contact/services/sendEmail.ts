9c3218502948d7de96cd4df3070fbb1f22f861e0
000EZE000 <ezequiel.ignacio.sosa@gmail.com>
2023-02-08 14:37:00 -0300

    change rename file refactor sendeEmail

---
+++ components/contact/services/sendEmail.ts
@@ -0,0 +1,20 @@
+import axios, { AxiosResponse } from "axios";
+
+import { InterfaceForm } from "../models";
+
+type typeSendFormServer = (form: InterfaceForm) => Promise<"fail" | "success">;
+
+const sendFormServer: typeSendFormServer = async (form) => {
+  try {
+    const responseFromBackend: AxiosResponse = await axios({
+      method: "post",
+      url: "api/sendeEmail",
+      data: form,
+    });
+    return responseFromBackend.status === 200 ? "success" : "fail";
+  } catch (error) {
+    return "fail";
+  }
+};
+
+export default sendFormServer;
